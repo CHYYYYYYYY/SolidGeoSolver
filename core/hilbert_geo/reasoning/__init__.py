1e709a39c5193b585efaa8e6ad7ef8a203697e42
Ruoran Xu <Ruoran.Xu24@student.xjtlu.edu.cn>
2026-04-25 21:26:36 +0800

    Update copyright and author information

--- core/hilbert_geo/reasoning/__init__.py
+++ core/hilbert_geo/reasoning/__init__.py
@@ -1,6 +1,6 @@
-# Copyright (C) 2025 FormalSGeo Advenced Team
-# Author: Ruoran Xu
-# Contact: Ruoran.Xu24@student.xjtlu.edu.cn
+# Copyright (C) 2025 SolidGeoGeo Advenced Team
+# Author: Ruoran Xu, Haoyu Cheng
+# Contact: Ruoran.Xu24@student.xjtlu.edu.cn, Haoyu.Cheng22@student.xjtlu.edu.cn
 
 __all__ = [
     "GeometrySolver"
